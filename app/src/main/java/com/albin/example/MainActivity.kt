833066587318950bdd7a50be9307539b89bbdc22
Albin Sojan <albinsojan@MacBookAir.lan>
2025-05-19 14:32:09 +0530

    change

--- app/src/main/java/com/albin/example/MainActivity.kt
+++ app/src/main/java/com/albin/example/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello Albin My name is $name!",
+        text = "Hello ",
         modifier = modifier
     )
 }
